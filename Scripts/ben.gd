b07b851c75526fc269094e6faa298433031450b6
ColaLight <colalightwastaken@proton.me>
2026-02-11 13:25:01 +0100

    Added mobile/mouse support

--- Scripts/ben.gd
+++ Scripts/ben.gd
@@ -8,5 +8,10 @@ func _ready() -> void:
 var direction : Vector2
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	direction = Input.get_vector("left", "right", "up", "down") * delta * 700
-	$".".position.x = clamp(position.x+direction.x, 50, 1100)
+	if $"../ButtonLeft".button_pressed:
+		$".".position.x = clamp(position.x-3.88, 50, 1100)
+	elif $"../ButtonRight".button_pressed:
+		$".".position.x = clamp(position.x+3.88, 50, 1100)
+	else:
+		direction = Input.get_vector("left", "right", "up", "down") * delta * 700
+		$".".position.x = clamp(position.x+direction.x, 50, 1100)
